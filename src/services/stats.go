711fdf1e244efdb7b29135de12bcfad43fd1b931
Edwin Garcia <spark.com.co@gmail.com>
2021-03-26 04:54:40 -0500

    [feature] database conection and get stats

--- src/services/stats.go
+++ src/services/stats.go
@@ -1,11 +1,30 @@
 package services
 
-import "github.com/edwintrumpet/mercado-libre-test/src/schema"
+import (
+	"fmt"
+
+	"github.com/edwintrumpet/mercado-libre-test/src/database"
+	"github.com/edwintrumpet/mercado-libre-test/src/schema"
+)
 
 func Stats() schema.Stats {
 	var stats schema.Stats
-	stats.CountMutantDna = 40
-	stats.CountHumanDna = 100
-	stats.Ratio = stats.CountMutantDna / stats.CountHumanDna
+	response, err := database.StatsGet()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	m := make(map[string]int)
+	for _, v := range response {
+		m[v.Name] = v.Value
+	}
+	stats.CountHumanDna = float32(m["count_human_dna"])
+	stats.CountMutantDna = float32(m["count_mutant_dna"])
+	if stats.CountHumanDna != 0 {
+		stats.Ratio = stats.CountMutantDna / stats.CountHumanDna
+	} else {
+		stats.Ratio = stats.CountMutantDna
+	}
+
 	return stats
 }
